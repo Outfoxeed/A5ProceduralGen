fbb423b5afcbedd777b7e5737e2b5755ca59b431
Outfoxeed <letourneaumorigan@gmail.com>
2025-01-21 09:50:07 +0100

    🚀 HallwayRoom script overriding doors spawn

--- Game/Rooms/Tests/test_spawn_room.gd
+++ Game/Rooms/Tests/test_spawn_room.gd
@@ -1,23 +1,32 @@
 extends Node2D
 
 @export var room_to_spawn : PackedScene
+@export var hallway_to_spawn : PackedScene
 
 @export_category("RoomObject spawn")
 @export var wanted_room_object : PackedScene = null
 @export var wanted_room_object_amount : int = 1
 
 func _ready() -> void:
-	var room : Room = room_to_spawn.instantiate()
-	room.global_position = Vector2.ZERO
+	_spawn_room(room_to_spawn, Vector2.ZERO)
+	_spawn_room(hallway_to_spawn, Vector2(0, 300))
 	
-	print("[TEST SPAWN ROOM] Before add_child: \n" + str(room.spawn_points))
+func _spawn_room(room_scene: PackedScene, room_pos: Vector2) -> void:
+	var room : Room = room_scene.instantiate()
+	room.global_position = room_pos
+	
+	#print("[TEST SPAWN ROOM] Before add_child: \n" + str(room.spawn_points))
 	await get_tree().process_frame
-	print("[TEST SPAWN ROOM] Before add_child + wait a frame: \n" + str(room.spawn_points))
+	#print("[TEST SPAWN ROOM] Before add_child + wait a frame: \n" + str(room.spawn_points))
 	
 	self.add_child(room)
 	
-	print("[TEST SPAWN ROOM] After add_child: \n" + str(room.spawn_points))
+	#print("[TEST SPAWN ROOM] After add_child: \n" + str(room.spawn_points))
 	await get_tree().process_frame
-	print("[TEST SPAWN ROOM] After add_child + wait frame: \n" + str(room.spawn_points))
+	#print("[TEST SPAWN ROOM] After add_child + wait frame: \n" + str(room.spawn_points))
 	
 	room.request_spawn(wanted_room_object, wanted_room_object_amount)
+	var rng : RandomNumberGenerator = RandomNumberGenerator.new()
+	room.spawn_doors(rng.randi() % 2 == 0, rng.randi() % 2 == 0, 
+	rng.randi() % 2 == 0, rng.randi() % 2 == 0)
+	#room.spawn_doors(true, true, true, true)
---
+++ Game/Rooms/hallway_room.gd
@@ -0,0 +1,33 @@
+class_name HallwayRoom extends Room
+
+func spawn_doors(top: bool, right: bool, down: bool, left: bool) -> void:
+	var rect : Rect2i = tilemap_layers[0].get_used_rect()
+	var start_cell_pos : Vector2i = rect.position
+	
+	if top:
+		_delete_cells(start_cell_pos + Vector2i(1,0), Vector2i.RIGHT, rect.size.x - 2)
+	if right:
+		_delete_cells(start_cell_pos + Vector2i(rect.size.x - 1, 1), Vector2i.DOWN, rect.size.y - 2)
+	if down:
+		_delete_cells(start_cell_pos + Vector2i(1, rect.size.y - 1), Vector2i.RIGHT, rect.size.x - 2)
+	if left:
+		_delete_cells(start_cell_pos + Vector2i(0, 1), Vector2i.DOWN, rect.size.y - 2)
+
+	# We don't want to delete the corners and so we commented out this code
+	#if top and left:
+		#_delete_cell(start_cell_pos)
+	#if top and right:
+		#_delete_cell(start_cell_pos + Vector2i(rect.size.x - 1, 0))
+	#if down and left:
+		#_delete_cell(start_cell_pos + Vector2i(0, rect.size.y - 1))
+	#if down and right:
+		#_delete_cell(start_cell_pos + rect.size - Vector2i.ONE)
+
+func _delete_cell(cell_pos: Vector2i) -> void:
+	for tilemap_layer in tilemap_layers:
+		tilemap_layer.set_cell(cell_pos, door_source_id, 
+		 door_atlas_coord, door_alternative_tile)
+	
+func _delete_cells(start: Vector2i, step: Vector2i, step_amount: int) -> void:
+	for i in range(0, step_amount):
+		_delete_cell(start + step * i)
